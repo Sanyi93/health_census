5316e6cf38b6d16c409c83430e84061ae08f09bc
Sanyi93 <sandor.tomas93@gmail.com>
2025-01-31 08:21:15 -0500

    addPatient function added

--- health_analysis.js
+++ health_analysis.js
@@ -2,3 +2,19 @@ const addPatientButton = document.getElementById("addPatient");
 const report = document.getElementById("report");
 const btnSearch = document.getElementById('btnSearch');
 const patients = [];
+
+function addPatient(){
+    //defining the variables - value of the input fields
+    const name = document.getElementById("name").value;
+    //the checked radioButton
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const age = document.getElementById("age").value;
+    const condition = document.getElementById("condition").value;
+
+    if(name && gender && age && condition){
+        patients.push({name, gender: gender.value, age, condition})
+        //the following methods to be done
+        resetForm();
+        generateReport();
+    }
+}
